4f056593fc150f4cb9d823334be01f03c4623e84
Dennis Olsen <dennan_lol@hotmail.com>
2025-08-12 14:48:26 +0200

    Lecture 2

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,39 @@ const App = () => {
 
   return (
     <>
+      {/*
       <Navbar />
       <Content />
+      <StudentTable />
+      */}
+
+      <div className="container">
+        <h2>state ex 1</h2>
+        <Counter />
+        <h2>state ex 2</h2>
+        <LikeButton />
+        <h2>state ex 3</h2>
+        <PersonForm />
+
+        <h2>props ex 1</h2>
+        <AlertMessage
+          message="Operation is Done."
+          alertType="alert-success"
+          icon={<FaCheckCircle />}
+        />
+        <AlertMessage
+          message="Error occured during operation!"
+          alertType="alert-danger"
+          icon={<FaExclamationTriangle />}
+        />
+        <AlertMessage
+          message="Please review befor proceeding."
+          alertType="alert-warning"
+        />
+
+        <h2>props ex 2</h2>
+        <Pricing />
+      </div>
     </>
   );
 };
